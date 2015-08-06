5d830d7d06ae3fdac1891fcc4ef7ad398cb86226
TopUrosevich <topurosevich@gmail.com>
2015-08-06 16:02:26 +0200

    update 1-0

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,5 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
-
 angular.module('F1FeederApp', [
     'F1FeederApp.controllers',
     'F1FeederApp.services',
