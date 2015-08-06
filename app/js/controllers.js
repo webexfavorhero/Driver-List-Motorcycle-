362a680ac9f2c53c0cb4637afbe81d3f8482a922
TopUrosevich <topurosevich@gmail.com>
2015-08-06 16:20:58 +0200

    update 1-4

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,8 +8,8 @@ angular.module('F1FeederApp.controllers', []).
         $scope.nameFilter = null;
         $scope.driversList = [];
         $scope.searchFilter = function (driver) {
-            var re = new RegExp($scope.nameFilter, 'i');
-            return !$scope.nameFilter || re.test(driver.Driver.givenName) || re.test(driver.Driver.familyName);
+            var keyword = new RegExp($scope.nameFilter, 'i');
+            return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
         };
 
         ergastAPIservice.getDrivers().success(function (response) {
